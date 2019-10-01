89e3e4ae6d0e0f34c1e7721720f7838f4c4760cb
Poddevin Romain <poddevin.romain@gmail.com>
2019-10-01 15:03:44 +0200

    2nd commit - 5 exercices added

---
+++ exo_03.rb
@@ -0,0 +1,2 @@
+puts " Hello Monde "
+#puts " Et avec une voix sexy, ça donne : Bonjour monde !"
---
+++ exo_05.rb
@@ -0,0 +1,28 @@
+# inidque d'afficher la suite de caractère (string): On va compter le nombre d'heures de travail à THP
+puts "On va compter le nombre d'heures de travail à THP"
+# indique d'afficher la suite de caractère(string): Travail puis d'évaluer le contenu de #{} et d'en afficher le résultat
+puts "Travail : #{10 * 5 * 11}"
+# indique d'afficher la suite de caractère(string): En minutes ça fait puis d'évaluer le contenu de #{} et d'en afficher le résultat
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+# inidque d'afficher la suite de caractère (string): Et en secondes ?
+puts "Et en secondes ?"
+# effectuer une multiplication des nombres indiqués
+puts 10 * 5 * 11 * 60 * 60
+# inidque d'afficher la suite de caractère (string): Est-ce que c'est vrai que 3 + 2 < 5 - 7
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+# Affiche le résultat du boléen visant à définir si il est vrai ou faux que 3 + 2 < 5 - 7 
+puts 3 + 2 < 5 - 7
+# inidque d'afficher la suite de caractère (string): Ça fait combien 3 + 2 ?  puis évalue le contenu de #{} et en affiche le résultat
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+# inidque d'afficher la suite de caractère (string): Ça fait combien 5 - 7 ?  puis évalue le contenu de #{} et en affiche le résultat
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+# inidque d'afficher la suite de caractère (string): Ok, c'est faux alors !
+puts "Ok, c'est faux alors !"
+# inidque d'afficher la suite de caractère (string): C'est drôle ça, faisons-en plus :
+puts "C'est drôle ça, faisons-en plus :"
+# inidque d'afficher la suite de caractère (string): Est-ce que 5 est plus grand que -2 ?  puis évalue le contenu de #{} et en affiche le résultat
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+# inidque d'afficher la suite de caractère (string): Est-ce que 5 est supérieur ou égal à -2 ? puis évalue le contenu de #{} et en affiche le résultat
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+# inidque d'afficher la suite de caractère (string): Est-ce que 5 est inférieur ou égal à -2 ?  puis évalue le contenu de #{} et en affiche le résultat
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
